c8ee955812b4e8d672ce59b4ac37160f0cf7c21d
Samuk <marcelolamas19@gmail.com>
2024-03-25 19:33:05 -0300

    Install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from 'astro/config';
 import unocss from '@unocss/astro';
 import presetWind from '@unocss/preset-wind';
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import transformerVariantGroup from '@unocss/transformer-variant-group';
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss({
-      injectReset: true,
-      presets: [
-        presetWind()
-      ],
-      transformers: [
-        transformerVariantGroup()
-      ]
-    }),
-  ]
+  integrations: [unocss({
+    injectReset: true,
+    presets: [presetWind()],
+    transformers: [transformerVariantGroup()]
+  }), preact()]
 });
