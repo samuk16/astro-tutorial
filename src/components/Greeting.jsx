e0f687d8d9f8c3a4722075bcea31186030861b93
Samuk <marcelolamas19@gmail.com>
2024-03-26 18:05:43 -0300

    Add dark mode

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({messages}) {
   return (
     <div class={'outline outline-1 outline-slate-500 flex w-fit flex-col p-4 rounded'}>
       <h3 class={'w-fit'}>{greeting}! Thank you for visiting!</h3>
-      <button class={'rounded p-1 hover:(bg-slate-300 text-slate-800) bg-slate-700'} onClick={() => setGreeting(randomMessage())}>
+      <button class={'rounded p-1 hover:(bg-slate-300 text-slate-800) bg-slate-700 btnGreeting'} onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
     </div>
